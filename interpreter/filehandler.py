93accec4d879cdc33193873189047170c6135e46
Wesley <Bustahboi@live.com>
2018-05-31 21:06:34 +1200

    Template Method
    Created template method for each type of file reader, consists of multiple steps to produce different output

--- interpreter/filehandler.py
+++ interpreter/filehandler.py
@@ -4,181 +4,137 @@ from csv import DictReader as CSVDictReader
 from openpyxl import load_workbook, utils
 from datetime import datetime, date
 from interpreter.validator import Validator
-from interpreter import validator
 
 
-# James
 class FileHandler:
-    # James
     def __init__(self):
         self.filename = None
         self.file_type = None
 
     def load(self, filename):
         self.filename = filename
-        self.set_file_type()
 
-    # James
-    def file_exist(self):
-        """
-        Checks if the current file exists
-        """
-        if self.filename.exists():
-            return True
-        else:
-            return False
-
-    # Wesley
-    def set_file_type(self):
-        """
-        Will get the file type and will create the
-        corresponding solid class and set it to self.file_type
-        """
+    def set_file(self):
         suffix = PurePosixPath(self.filename).suffix
         print(suffix)
         file_types = {
-            '.csv': FileTypeCSV(),
-            '.xlsx': FileTypeXLSX(),
-            '.txt': FileTypeTXT()
+            '.csv': CSVReader(self.filename),
+            '.xlsx': XLSXReader(self.filename),
+            '.txt': TXTReader(self.filename)
         }
         self.file_type = file_types[suffix]
 
-    def read(self):
-        return self.file_type.read(self.filename)
+    def get_process_data(self):
+        self.set_file()
+        self.file_type.do_process_data()
+        return self.file_type.get_file()
 
 
-# Wesley
-class FileTypeAbstract(metaclass=ABCMeta):
-    # Wesley
+class FileReader(metaclass=ABCMeta):
+    def __init__(self, filename):
+        self.filename = filename
+        self.file = None
+
+    def do_process_data(self):
+        self.load_document()
+        self.get_file_data()
+        self.parse_data()
+        self.validate_data()
+
+    # hook operation
+    def load_document(self):
+        self.file = open(self.filename)
+
     @abstractmethod
-    def read(self, filename):
+    def get_file_data(self):
         pass
 
+    @abstractmethod
+    def parse_data(self):
+        pass
 
-# Wesley
-class FileTypeCSV(FileTypeAbstract):
-    # James
-    def read(self, filename):
-        """
-        Return dictionary with key => value pairs
-        :param filename is the file where the values exist
-        >>> read("Saves/data.csv")
-        """
-        # try:
-        data = dict()
-        empno = 0
-        with open(filename) as f:
-            reader = CSVDictReader(f)
-            for row in reader:
-                record = dict()
-                for key in row:
-                    record[key] = row.get(key)
-                data[empno] = record
-                empno += 1
-            # print(data)
-        # James' changes (13/03)
-        result = Validator.save_dict(data)
-        print(result)
-        return result
-        # except TypeError:
-        #     print("Error!!")
+    def validate_data(self):
+        validate = Validator()
+        self.file = validate.save_dict(self.file)
+
+    def get_file(self):
+        return self.file
 
 
-# Wesley
-class FileTypeXLSX(FileTypeAbstract):
-    # Wesley
-    def read(self, filename):
-        """
-        Return dictionary with key => value pairs
-        :param filename is the file where the values exist
-        >>> f = FileTypeXLSX()
-        >>> f.read("Saves/doctest.xlsx")
-        {1: {'ID': 'G262', 'Gender': 'Female', 'Age': 12, 'Sales': 215, 'BMI': 'Normal', 'Salary': 23, 'Birthday': '24-05-1993'}, 2: {'ID': 'A233', 'Gender': 'Chihuahua', 'Age': 22, 'Sales': 245, 'BMI': 'normal', 'Salary': 23, 'Birthday': '24-06-1995'}, 3: {'ID': 'A262', 'Gender': 'M', 'Age': 24, 'Sales': 845, 'BMI': 'Normal', 'Salary': 23, 'Birthday': '24-05-1993'}, 4: {'ID': 'A233', 'Gender': 'Female', 'Age': 62, 'Sales': 245, 'BMI': 'normal', 'Salary': 23, 'Birthday': '24-06-1995'}}
-        Adding Row 1
-        {'ID': 'G262', 'Gender': 'F', 'Age': '12', 'Sales': '215', 'BMI': 'Normal', 'Salary': '23', 'Birthday': '24-05-1993'}
-        Error at entry: 2
-        Adding Row 3
-        {'ID': 'A262', 'Gender': 'M', 'Age': '24', 'Sales': '845', 'BMI': 'Normal', 'Salary': '23', 'Birthday': '24-05-1993'}
-        Adding Row 4
-        {'ID': 'A233', 'Gender': 'F', 'Age': '62', 'Sales': '245', 'BMI': 'Normal', 'Salary': '23', 'Birthday': '24-06-1995'}
-        result
-        {1: {'ID': 'G262', 'Gender': 'F', 'Age': '12', 'Sales': '215', 'BMI': 'Normal', 'Salary': '23', 'Birthday': '24-05-1993'}, 3: {'ID': 'A262', 'Gender': 'M', 'Age': '24', 'Sales': '845', 'BMI': 'Normal', 'Salary': '23', 'Birthday': '24-05-1993'}, 4: {'ID': 'A233', 'Gender': 'F', 'Age': '62', 'Sales': '245', 'BMI': 'Normal', 'Salary': '23', 'Birthday': '24-06-1995'}}
-            """
-        data = dict()
-        empno = 0
-        keys = []
-        a_row = 0
-        try:
-            workbook = load_workbook(filename)
-            first_sheet = workbook.sheetnames[0]
-            worksheet = workbook[first_sheet]
-            for row in worksheet.iter_rows():
-                record = dict()
-                row_num = 0
+class XLSXReader(FileReader):
+
+    def load_document(self):
+        self.file = load_workbook(self.filename)
+        sheet_name = self.file.sheetnames[0]
+        self.file = self.file[sheet_name]
+
+    def get_file_data(self):
+        result = list()
+        for row in self.file.rows:
+            result_row = list()
+            for cell in row:
+                result_row.append(cell.value)
+            result.append(result_row)
+        self.file = result
+
+    def parse_data(self):
+        result = dict()
+        titles = self.file[0]
+        row_num = 1
+        for row in self.file:
+            counter = 0
+            if row is not self.file[0]:
+                result_row = dict()
                 for cell in row:
-                    a_row = cell.row
-                    if 1 == a_row:
-                        keys.append(cell.value)
-                    else:
-                        valid = cell.value
-                        if isinstance(cell.value, datetime):
-                            valid = Validator.xlsx_date(cell.value)
-                        record[keys[row_num]] = valid
-                    row_num += 1
-                if a_row > 1:
-                    data[empno] = record
-                empno += 1
-            # print(data)
-            result = Validator.save_dict(data)
-            validator.a = None
-        except PermissionError:
-            result = dict()
-            print("Sorry, you don't have enough permissions to access this file")
-        print("result")
-        return result
-# The above function contains a date object in the dictionary for each date,
-# as the birthday is a date, may need to access the values stored in the date object when validating
+                    result_row[titles[counter]] = cell
+                    if isinstance(cell, datetime):
+                        cell = Validator.xlsx_date(cell)
+                    result_row[titles[counter]] = cell
+                    counter = counter + 1
+                result[row_num] = result_row
+                row_num = row_num + 1
+        self.file = result
 
 
-# Sam
-class FileTypeTXT(FileTypeAbstract):
-    def read(self, filename):
-        """
-        Return dictionary with key => value pairs
-        :param filename is the file where the values exist
-        >>> read("Saves/data.txt")
-        """
-        empno = 0
-        try:
-            file = open(filename, 'r')
-            print(file)
-            for line in file:
-                print(line)
-                dictionary = dict()
-                rows = line.split(":")
-                for row in rows:
-                    if len(row.split("=")) == 2:
-                        key = row.split("=")[0]
-                        value = row.split("=")[1]
-                        value = value.rstrip('\n')
-                        dictionary[key] = value
-                    else:
-                        print("File error")
-                        return False
-            return dictionary
-        finally:
-            print("something weird happened... guys pls send help")
+class CSVReader(FileReader):
+
+    def get_file_data(self):
+        self.file = CSVDictReader(self.file)
+
+    def parse_data(self):
+        result = dict()
+        row_num = 1
+        for row in self.file:
+            result_row = dict()
+            for key in row:
+                result_row[key] = row.get(key)
+            result[row_num] = result_row
+            row_num = row_num + 1
+        self.file = result
 
 
-# def run():
-#     a = FileHandler.get_file_name()
-#     aclass = FileHandler(a)
-#     while aclass.file_exist() is False:
-#         print("File exists:", aclass.file_exist())
-#         a = FileHandler.get_file_name()
-#         aclass = FileHandler(a)
-#     aclass.set_file_type()
-#     aclass.read()
+class TXTReader(FileReader):
 
+    def get_file_data(self):
+        lines = list()
+        for row in self.file:
+            items = row.split(',')
+            items[6] = items[6].replace('\n', '')
+            lines.append(items)
+        self.file = lines
 
-# run()
+    def parse_data(self):
+        result = dict()
+        row_num = 1
+        titles = self.file[0]
+        for row in self.file:
+            if row is not self.file[0]:
+                title = 0
+                result_row = dict()
+                for item in row:
+                    result_row[titles[title]] = item
+                    title += 1
+                result[row_num] = result_row
+                row_num += 1
+        self.file = result
+
